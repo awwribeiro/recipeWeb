3ecc5b1b19f4011a5074293ee4fae5fce20beb6d
Ana <awwribeiro@gmail.com>
2025-05-28 09:35:35 -0300

    criando componente ingredientes

--- src/app/componentes/receitas/receita-completa/ingredientes/ingredientes.component.ts
+++ src/app/componentes/receitas/receita-completa/ingredientes/ingredientes.component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import { Receita } from '../../receita';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ReceitaService } from '../../receita.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-ingredientes',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './ingredientes.component.html',
   styleUrl: './ingredientes.component.css'
 })
 export class IngredientesComponent {
+  dados!: Receita;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private receitaService: ReceitaService) {}
+
+  private isOutletAtivo(nomeOutlet: string): boolean {
+    const snapshot = this.router.routerState.snapshot; //guarda a estrutura completa das rotas carregadas no momento
+    //.routerState acessa o estado atual das rotas
+    //.snapshot captura um instantaneo estativo do estado atual da árvore de rotas
+    return snapshot.root.children.some(child => child.outlet === nomeOutlet)
+  }
+
+  verListaCompras(){
+    const estaAberto = this.isOutletAtivo('receitaFav');
+    if(estaAberto){ //se o componente receitasFav esta aberto
+        this.router.navigate([{ outlets: { receitaFav: null } }]) // Fecha o outlet atual
+        .then(() => {
+          this.router.navigate([{ outlets: { receitaFav: ['lista-ingredientes'] } }]); // Abre o novo componente no mesmo outlet
+        });
+    } else {
+        this.router.navigate([{ outlets: { receitaFav: ['lista-ingredientes'] } }]); //abre o componente listaIngredientes
+    }
+  }
 
 }
--- src/app/componentes/receitas/receita-completa/receita-completa.component.ts
+++ src/app/componentes/receitas/receita-completa/receita-completa.component.ts
@@ -5,10 +5,11 @@ import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { ReceitaService } from '../receita.service';
 import { CardComponent } from '../card/card.component';
 import { ApresentacaoComponent } from './apresentacao/apresentacao.component';
+import { IngredientesComponent } from "./ingredientes/ingredientes.component";
 
 @Component({
   selector: 'app-receita-completa',
-  imports: [CommonModule, CardComponent, RouterModule, ApresentacaoComponent],
+  imports: [CommonModule, CardComponent, RouterModule, ApresentacaoComponent, IngredientesComponent],
   templateUrl: './receita-completa.component.html',
   styleUrl: './receita-completa.component.css'
 })
@@ -19,26 +20,6 @@ export class ReceitaCompletaComponent {
     private route: ActivatedRoute,
     private router: Router,
     private receitaService: ReceitaService) {}
-
-
-  private isOutletAtivo(nomeOutlet: string): boolean {
-    const snapshot = this.router.routerState.snapshot; //guarda a estrutura completa das rotas carregadas no momento
-    //.routerState acessa o estado atual das rotas
-    //.snapshot captura um instantaneo estativo do estado atual da árvore de rotas
-    return snapshot.root.children.some(child => child.outlet === nomeOutlet)
-  }
-
-  verListaCompras(){
-    const estaAberto = this.isOutletAtivo('receitaFav');
-    if(estaAberto){ //se o componente receitasFav esta aberto
-        this.router.navigate([{ outlets: { receitaFav: null } }]) // Fecha o outlet atual
-        .then(() => {
-          this.router.navigate([{ outlets: { receitaFav: ['lista-ingredientes'] } }]); // Abre o novo componente no mesmo outlet
-        });
-    } else {
-        this.router.navigate([{ outlets: { receitaFav: ['lista-ingredientes'] } }]); //abre o componente listaIngredientes
-    }
-  }
 
 
 }
